112f2f31be11aa43e334f24be5709e475d13ad69
Albarracing <109823598+Albarracing@users.noreply.github.com>
2022-09-30 16:47:43 -0300

    Delete OneDrive/Documentos/SistemaDeGestion/src directory

--- OneDrive/Documentos/SistemaDeGestion/src/front/routes/helperRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Route, Redirect } from 'react-router-dom';
-
-const PublicRoute = ({componen, ...options}) => {
-    const isAuth = false;
-    if (!isAuth) return <Route {...options} component={componen}/>
-    return <Redirect to="/"/>
-}
-
-export default PublicRoute;
